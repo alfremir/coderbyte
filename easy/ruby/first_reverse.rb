f431f70cc43201a7712d69ed02bb6002be3e7a1f
Alfredo Miranda <alfredo@miranda.io>
2014-01-04 23:04:05 -0600

    Completed first reverse problem

---
+++ easy/ruby/first_reverse.rb
@@ -0,0 +1,9 @@
+# Using the Ruby language, have the function FirstReverse(str) take the str 
+# parameter being passed and return the string in reversed order.
+def FirstReverse(str)
+    return str.reverse!
+end
+                
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down
+FirstReverse(STDIN.gets)  
