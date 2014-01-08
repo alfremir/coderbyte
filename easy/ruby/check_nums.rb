e514e12d4addc8df7bed4e643ca90fa6813f2847
Alfredo Miranda <alfredo@miranda.io>
2014-01-08 14:28:27 -0600

    Completed Check Nums

---
+++ easy/ruby/check_nums.rb
@@ -0,0 +1,18 @@
+# Have the function CheckNums(num1,num2) take both parameters being passed and 
+# return the string true if num2 is greater than num1, otherwise return the 
+# string false. If the parameter values are equal to each other then return 
+# the string -1.
+def CheckNums(num1,num2)
+  if num2 > num1
+    return "true"
+  end
+          
+  if num2 < num1
+    return "false"
+  end
+                      
+  return "-1"
+end
+
+# keep this function call here 
+CheckNums(STDIN.gets)
