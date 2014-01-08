5a6a5c6b3ba6f75d0392755ead1f7eeb6100f14a
Alfredo Miranda <alfredo@miranda.io>
2014-01-08 11:24:53 -0600

    Completed Longest Word

---
+++ easy/ruby/longest_word.rb
@@ -0,0 +1,19 @@
+# Using the Ruby language, have the function LongestWord(sen) take the sen 
+# parameter being passed and return the largest word in the string. If there 
+# are two or more words that are the same length, return the first word from 
+# the string with that length. Ignore punctuation and assume sen will not be 
+# empty.
+def LongestWord(sen)
+  longest = ""
+
+  sen.scan(/\w+/) do |word|
+    if word.length > longest.length
+      longest = word
+    end
+  end
+                      
+  return longest
+end
+                           
+# keep this function call here 
+LongestWord(STDIN.gets)  
