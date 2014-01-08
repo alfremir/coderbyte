205e41f0aaa782b97ed41aaa3131b96ab132d143
Alfredo Miranda <alfredo@miranda.io>
2014-01-08 15:21:49 -0600

    Completed Word Count

---
+++ easy/ruby/word_count.rb
@@ -0,0 +1,9 @@
+# Have the function WordCount(str) take the str string parameter being passed 
+# and return the number of words the string contains (ie. "Never eat shredded 
+# wheat" would return 4). Words will be separated by single spaces.
+def WordCount(str)
+  return str.split.length
+end
+       
+# keep this function call here 
+WordCount(STDIN.gets)
