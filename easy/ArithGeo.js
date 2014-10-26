f8cdfa674f8bf745b56ee27c4a75752c1aba1897
Alfredo Miranda <alfredo@miranda.io>
2014-10-25 23:00:33 -0500

    Completed ArithGeo

---
+++ easy/ArithGeo.js
@@ -0,0 +1,27 @@
+function ArithGeo(arr) {
+  var arith = true;
+  var geo = true;
+  var diff = arr[1] - arr[0];
+  var ratio = arr[1] / arr[0];
+
+  
+  for(var i = 2; i < arr.length; i++) {
+    if(arr[i] - arr[i-1] != diff) {
+      arith = false;
+    }
+    
+    if(arr[i] / arr[i-1] != ratio) {
+      geo = false;
+    }
+  }
+  
+  if(arith) {
+    return 'Arithmetic';
+  } else if(geo) {
+    return 'Geometric';
+  }
+  
+  return -1;
+}
+  
+ArithGeo(readline());
